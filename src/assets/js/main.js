5edc96c929aaadc8b1d09f75292b94c780656fbd
FabryPostRock <fabriziodemasi01@gmail.com>
2026-04-22 12:26:37 +0200

    found a bug in hacker_news_api

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,72 @@
 import { HackerNewsAPI } from './hacker_news_api.js';
+import { News } from './observable.js';
+import { renderNewsChange } from './observers.js';
+
+const STORAGE_NEWS = {
+  kItemsIds: 'news:itemsIds',
+  kIdxStartId: 'news:idxStartId',
+};
+
+async function getNewsBlock(obs, { kItemsIds, kIdxStartId }) {
+  const NUM_NEWS_BLOCK = 10;
+  let itemsIds = null;
+  let setTid = null;
+  if (kItemsIds) {
+    itemsIds = JSON.parse(sessionStorage.getItem(kItemsIds));
+  } else {
+    return;
+  }
+  // itemsIds dev'essere un array
+  if (itemsIds?.data && Object.getPrototypeOf(itemsIds?.data) === Array.prototype) {
+    let idxStartId = null;
+    // verifico se è già stato salvato l'indice di partenza del blocco di news
+    if (sessionStorage.getItem(kIdxStartId)) {
+      idxStartId = sessionStorage.getItem(kIdxStartId);
+    } else {
+      idxStartId = 0;
+    }
+    let idxEndId = null;
+    // stabilisco l'indice finale del blocco di news
+    if (idxStartId + NUM_NEWS_BLOCK - 1 > itemsIds.data.length) {
+      idxEndId = itemsIds.data.length;
+    } else {
+      idxEndId = idxStartId + NUM_NEWS_BLOCK - 1;
+    }
+
+    for (let i = idxStartId; i < idxEndId; i++) {
+      console.log('AAAAAA');
+      let newsData = null;
+      /*setTid = setTimeout(async () => {
+        newsData = await HackerNewsAPI.getBlockNewsDetails(itemsIds.data[i]);
+        obs.subscribe(renderNewsChange);
+        if (newsData?.data) obs.addNews(newsData.data);
+        console.log('BBBBBBBB');
+      }, 2000);*/
+      newsData = await HackerNewsAPI.getBlockNewsDetails(itemsIds.data[i]);
+      //obs.subscribe(renderNewsChange);
+      //if (newsData?.data) obs.addNews(newsData.data);
+      console.log('CCCCCCCCC');
+      console.log('newsData', newsData.data.by);
+    }
+    if (idxEndId + 1 < itemsIds.length) {
+      sessionStorage.setItem(kIdxStartId, idxEndId + 1);
+    }
+    clearTimeout(setTid);
+    console.log('DDDDDDDDDD');
+  } else {
+    throw new Error('Could not retrive news Ids!');
+  }
+}
 
 try {
-  const itemsIds = await HackerNewsAPI.getAllNewsIDs(HackerNewsAPI.itemsIds.URL);
-  const newsData = await HackerNewsAPI.getBlockNewsDetails(itemsIds.data[0]);
-  console.log(res.data);
+  document.addEventListener('DOMContentLoaded', async () => {
+    const itemsIds = await HackerNewsAPI.getAllNewsIDs(HackerNewsAPI.itemsIds.URL);
+    // Oggetto da salvare in storage come stringa
+    sessionStorage.setItem(STORAGE_NEWS.kItemsIds, JSON.stringify(itemsIds));
+  });
+
+  const n = new News();
+  await getNewsBlock(n, STORAGE_NEWS);
 } catch (err) {
   console.error(err);
 }
