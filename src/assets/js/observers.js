f54581a8a7c02634d987394717f0295bf58b69ab
FabryPostRock <fabriziodemasi01@gmail.com>
2026-05-04 14:15:37 +0200

    some news' text content is html and some is not so i wrapped it into a div. I have also assigned classes to manage the news expansion.

--- src/assets/js/observers.js
+++ src/assets/js/observers.js
@@ -1,30 +1,69 @@
 /*
-CREATION OF THIS HTML CONTENT
-        <div class="card col-12 col-md-4 col-lg-3 w-max-18-rem m-2" data-news-id = 123213>
-          <div class="card-body">
-            <h5 class="card-title secondary-color">Card title</h5>
-            <!--<p class="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>-->
-            <div class="mb-3 d-flex justify-content-between">
-              <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
-                <small class="text-muted mb-0 lh-1">Ciccio Pasticcio</small>
-              </div>
-              <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
-                <span class="secondary-color lh-1">100</span>
-              </div>
-            </div>
-            <div class="mb-3 d-flex justify-content-between">
-              <a href="https://test.com" class="btn btn-secondary" target="_blank">Card link</a>
-              <div class="d-flex align-items-end gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
-                <small class="text-muted mb-0 lh-1">Fri 5 April 2024</small>
-              </div>
-            </div>
+NEWS TEMPLATE WITH ONLY URL
+
+<div class="news-item" data-news-id="48005744">
+  <div class="card flex-fill">             
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title secondary-color">Jaron Lanier on the Zeitgeist</h5>
+      <div class="mb-3 d-flex justify-content-between">
+          <div class="d-flex align-items-center gap-2">
+          <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
+          <small class="text-muted mb-0 lh-1">musha68k</small>
           </div>
-        </div>
+          <div class="d-flex align-items-center gap-2">
+          <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
+          <span class="secondary-color lh-1">1</span>
+          </div>
+      </div>
+      <div class="mt-auto mb-3 d-flex justify-content-between">
+          <a href="https://www.youtube.com/watch?v=Vmx3SbgoPZc" class="btn btn-secondary" target="_blank" rel="noopener noreferrer">Leggi</a>
+          <div class="d-flex align-items-end gap-2">
+            <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
+            <small class="text-muted mb-0 lh-1">Mon May 04 2026</small>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+NEWS TEMPLATE WITH TEXT
+<div class="news-item" data-news-id="48005705">
+  <div class="card flex-fill"> 
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title secondary-color">Everything needs to be open source. Why I decided to open source my project</h5>
+      <p class="card-text news-extra">
+          Everything needs to be open source. Why I decided to open source my project. and Why I think you should join the movement.
+      </p>
+      <p>I work in a security firm that belives everything needs to be open source.
+          Thats why I decided to open source our product. Hoping I will get a lot of love from the community and this will make other 
+          security firms consider open sourcing some of their products as well.
+      </p>
+      <p>open source will be good for the world. Give us love (and a github star ) if you want to help us in our mission to make 
+            the world more open source!
+            https://github.com/Cybereason-Public/owLSM
+      </p>
+      <p>
+        P.S I belive in karma. if I do good now (by giving this project to the community)god will help me in the future.<
+      /p>
+      <div class="mb-3 d-flex justify-content-between">
+          <div class="d-flex align-items-center gap-2">
+            <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
+            <small class="text-muted mb-0 lh-1">SilverPlate3</small>
+          </div>
+          <div class="d-flex align-items-center gap-2">
+            <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
+            <span class="secondary-color lh-1">1</span>
+          </div>
+      </div>
+      <div class="mt-auto mb-3 d-flex justify-content-between">
+          <div class="d-flex align-items-end gap-2">
+            <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
+            <small class="text-muted mb-0 lh-1">Mon May 04 2026</small>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 */
 
@@ -36,8 +75,9 @@ export function createNewsDomEl(news) {
   if (typeof news === 'object') {
     // data-news-id is important to identify the object again.
     const divCard = document.createElement('div');
-    divCard.className = 'card flex-fill col-12 col-md-4 col-lg-3 w-max-18-rem m-2 ';
+    divCard.className = 'news-item';
     divCard.setAttribute('data-news-id', news.id);
+
     if (news?.url) {
       /*
       flex-fill : definisce 
@@ -46,57 +86,53 @@ export function createNewsDomEl(news) {
       mt-auto : serve a strechare la parte bassa in modo che venga spinta in basso
       */
       divCard.innerHTML = `
-                         
-            <div class="card-body d-flex flex-column">
-            <h5 class="card-title secondary-color">${news.title}</h5>
-            <div class="mb-3 d-flex justify-content-between">
-                <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
-                <small class="text-muted mb-0 lh-1">${news.by}</small>
+            <div class="card flex-fill">             
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title secondary-color">${news.title}</h5>
+                <div class="mb-3 d-flex justify-content-between">
+                    <div class="d-flex align-items-center gap-2">
+                    <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
+                    <small class="text-muted mb-0 lh-1">${news.by}</small>
+                    </div>
+                    <div class="d-flex align-items-center gap-2">
+                    <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
+                    <span class="secondary-color lh-1">${news.score}</span>
+                    </div>
                 </div>
-                <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
-                <span class="secondary-color lh-1">${news.score}</span>
+                <div class="mt-auto mb-3 d-flex justify-content-between">
+                    <a href="${news.url}" class="btn btn-secondary" target="_blank" rel="noopener noreferrer">Leggi</a>
+                    <div class="d-flex align-items-end gap-2">
+                      <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
+                      <small class="text-muted mb-0 lh-1">${news.time}</small>
+                    </div>
                 </div>
-            </div>
-            <div class="mt-auto mb-3 d-flex justify-content-between">
-                <a href="${news.url}" class="btn btn-secondary" target="_blank" rel="noopener noreferrer">Leggi</a>
-                <div class="d-flex align-items-end gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
-                <small class="text-muted mb-0 lh-1">${news.time}</small>
-                </div>
-            </div>
+              </div>
             </div>
             `;
     } else if (news?.text) {
       divCard.innerHTML = `
+          <div class="card flex-fill"> 
             <div class="card-body d-flex flex-column">
-            <h5 class="card-title secondary-color">${news.title}</h5>
-            <p class="card-text">
-                ${news.text.slice(0, 100)}
-                <details>
-                    <summary>Mostra altro</summary>
-                    ${news.text.slice(100)}
-                </details>
-            </p>
-            <div class="mb-3 d-flex justify-content-between">
-                <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
-                <small class="text-muted mb-0 lh-1">${news.by}</small>
-                </div>
-                <div class="d-flex align-items-center gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
-                <span class="secondary-color lh-1">${news.score}</span>
-                </div>
+              <h5 class="card-title secondary-color">${news.title}</h5>
+              <div class="news-extra">${news.text}</div>
+              <div class="mb-3 d-flex justify-content-between">
+                  <div class="d-flex align-items-center gap-2">
+                    <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">article_person</span>
+                    <small class="text-muted mb-0 lh-1">${news.by}</small>
+                  </div>
+                  <div class="d-flex align-items-center gap-2">
+                    <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">thumb_up</span>
+                    <span class="secondary-color lh-1">${news.score}</span>
+                  </div>
+              </div>
+              <div class="mt-auto mb-3 d-flex justify-content-between">
+                  <div class="d-flex align-items-end gap-2">
+                    <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
+                    <small class="text-muted mb-0 lh-1">${news.time}</small>
+                  </div>
+              </div>
             </div>
-            <div class="mt-auto mb-3 d-flex justify-content-between">
-                 
-                <div class="d-flex align-items-end gap-2">
-                <span class="material-symbols-outlined g-icon-1em g-icon-secondary-color lh-1">calendar_month</span>
-                <small class="text-muted mb-0 lh-1">${news.time}</small>
-                </div>
-            </div>
-            </div>
+          </div>
             `;
     }
     return divCard;
