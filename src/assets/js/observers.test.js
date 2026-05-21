5bb68c162988b71b85525aa6ce73e1aa14a522b4
FabryPostRock <fabriziodemasi01@gmail.com>
2026-05-21 16:46:13 +0200

    added conditions to verify all types of HTML received

--- src/assets/js/observers.test.js
+++ src/assets/js/observers.test.js
@@ -5,7 +5,7 @@ import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 // jsdom simulates browser environment therefore we can access document object
 // @vitest-environment jsdom
 describe('createNewsDomEl', () => {
-  it('Return HTML OK: Se la news ha un url torna un elemento con un button', () => {
+  it('Return HTML with url OK: returns an html element with a button that opens an external page', () => {
     const item = {
       id: 2,
       by: 'Bob',
@@ -23,11 +23,12 @@ describe('createNewsDomEl', () => {
     expect(spy).toHaveReturned();
     expect(spy).toHaveReturnedWith(el);
     expect(el).toBeInstanceOf(HTMLDivElement);
-    expect(el.querySelector('a')).not.toBeNull();
     expect(el.querySelector('.news-extra')).toBeNull();
+    expect(el.querySelector('a[target]')).not.toBeNull();
+    expect(el.querySelector('a.btn.white.p-0.m-2.a-icon-hover')).not.toBeNull();
   });
 
-  it("Return HTML OK: L'elemento HTML torna con quello che ci si aspetta", () => {
+  it('Return HTML without url OK: returns an html element with an expandible text', () => {
     const item = {
       id: 2,
       by: 'Bob',
@@ -46,7 +47,30 @@ describe('createNewsDomEl', () => {
     expect(spy).toHaveReturnedWith(el);
     expect(el).toBeInstanceOf(HTMLDivElement);
     expect(el.querySelector('.news-extra')).not.toBeNull();
-    expect(el.querySelector('a')).toBeNull();
+    expect(el.querySelector('a[target]')).toBeNull();
+    expect(el.querySelector('a.btn.white.p-0.m-2.a-icon-hover')).not.toBeNull();
+  });
+
+  it('Return HTML without url and descriptive text OK: returns an html element without any link to external page', () => {
+    const item = {
+      id: 2,
+      by: 'Bob',
+      score: 87,
+      time: 'Fri Apr 17 2026',
+      title: 'Seconda news',
+      type: 'story',
+    };
+
+    // Ho dovuto importare il modulo intero per poter applicare spyOn
+    const spy = vi.spyOn(newsDomModule, 'createNewsDomEl');
+    const el = newsDomModule.createNewsDomEl(item);
+    // verifica che non generi eccezione e ritorni con successo qualcosa
+    expect(spy).toHaveReturned();
+    expect(spy).toHaveReturnedWith(el);
+    expect(el).toBeInstanceOf(HTMLDivElement);
+    expect(el.querySelector('.news-extra')).toBeNull();
+    expect(el.querySelector('a[target]')).toBeNull();
+    expect(el.querySelector('a.btn.white.p-0.m-2.a-icon-hover')).not.toBeNull();
   });
 
   it("Throw ERROR : L'elemento passato nei parametri non è di tipo Object", () => {
