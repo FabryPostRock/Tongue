22090e71ed3e0c2360655e755b1feb3e3e59172f
FabryPostRock <fabriziodemasi01@gmail.com>
2026-05-27 16:56:55 +0200

    changed base url with vite default url and configured config.js files to not mess Vite with Playwright.

--- vitest.config.js
+++ vitest.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
   },
   /* ATTENZIONE ALL'ORDINE DEGLI IMPORT*/
   test: {
+    // questa riga evita ad esempio che Vite faccia confusione con ad esempio questo :
+    // "import { test, expect } from '@playwright/test';" di Playwright che viene interpretato come
+    // suffisso '.test' da Vite
+    include: ['src/**/*.test.js'],
     environment: 'jsdom',
     // questo file mocka la clsse IntersectionObserver che è un'API del browser e in jsdom non esiste.
     setupFiles: ['./vitest.setup.js'],
